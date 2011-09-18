c70a9a834a413a9878adb3c214b5aeb0ad61bb1c
Sean Hodges <seanhodges84@gmail.com>
2011-09-18 15:23:02 +0100

    Added the thumb delete action

--- src/main/closure/thumbviewer.js
+++ src/main/closure/thumbviewer.js
@@ -1,19 +1,60 @@
 goog.provide('sean.test.ThumbViewer');
 
-sean.test.ThumbViewer = function(container) {
-    this.container_ = goog.dom.getElement(container);
+goog.require('goog.dom');
+goog.require('goog.ui.Component');
+
+sean.test.ThumbViewer = function(opt_domHelper) {
+    goog.ui.Component.call(this, opt_domHelper);
 };
 goog.inherits(sean.test.ThumbViewer, goog.ui.Component);
 
-sean.test.ThumbViewer.prototype.createDom = function() {
-    this.decorateInternal(this.container_);
+sean.test.ThumbViewer.prototype.decorateInternal = function(el) {
+    this.setElementInternal(el);
+    this.rowCount_ = 0;
+
+    // Clear the contents of the container
+    el.innerHTML = '';
 };
 
-sean.test.ThumbViewer.prototype.createRow = function(thumb_data) {
+sean.test.ThumbViewer.prototype.createRow = function(e) {
+    var caption = e.width + 'x' + e.height;
+
     // Create the row view
-    var row = goog.dom.createElement('div');
+    var row = goog.dom.createDom('div', { 'class' : 'thumb_viewer_row' });
+    var thumb = goog.dom.createDom('img', { 'src' : e.proxyUrl });
+    var caption = goog.dom.createDom('span', null, caption);
+    var hiddenField = goog.dom.createDom('input', {
+        'name' : 'contentData[thumbnails][' + this.rowCount_ + ']',
+        'type' : 'hidden',
+        'value' : e.value
+    });
+
+    goog.dom.appendChild(row, thumb);
+    goog.dom.appendChild(row, caption);
+    goog.dom.appendChild(row, hiddenField);
 
     // Add to the container
-    this.appendChild(row);
+    goog.dom.appendChild(this.getElement(), row);
+
+    this.rowCount_ += 1;
 };
 
+sean.test.ThumbViewer.prototype.deleteRow = function(id) {
+    rows = this.getElement().getElementsByTagName('div');
+    this.rowCount_ = 0;
+
+    for (i = 0; i < rows.length; i++) {
+        if (i == id) {
+            // Delete the target field
+            goog.dom.removeNode(rows[i]);
+        }
+        else {
+            // Re-index the other fields
+            var field = rows[id].getElementsByTagName('input')[0];
+            alert('before: ' + field.name);
+            field.name = 'contentData[thumbnails][' + this.rowCount_ + ']';
+            alert('after: ' + field.name);
+            this.rowCount_ += 1;
+        }
+    }
+};
