70b807bfffa403841611079c4b90bddb600dad36
Sean Hodges <seanhodges84@gmail.com>
2011-09-18 23:23:41 +0100

    Added the delete button click event

--- src/main/closure/thumbviewer.js
+++ src/main/closure/thumbviewer.js
@@ -1,6 +1,7 @@
 goog.provide('sean.test.ThumbViewer');
 
 goog.require('goog.dom');
+goog.require('goog.events');
 goog.require('goog.ui.Component');
 
 sean.test.ThumbViewer = function(opt_domHelper) {
@@ -23,14 +24,23 @@ sean.test.ThumbViewer.prototype.createRow = function(e) {
     var row = goog.dom.createDom('div', { 'class' : 'thumb_viewer_row' });
     var thumb = goog.dom.createDom('img', { 'src' : e.proxyUrl });
     var caption = goog.dom.createDom('span', null, caption);
+    var deleteButton = goog.dom.createDom('a', {
+        'id' : 'thumb_viewer_row_delete_' + this.rowCount_,
+        'href' : 'javascript:void(0)'
+    }, 'delete');
     var hiddenField = goog.dom.createDom('input', {
         'name' : 'contentData[thumbnails][' + this.rowCount_ + ']',
         'type' : 'hidden',
         'value' : e.value
     });
 
+    // Listeners
+    goog.events.listen(deleteButton, goog.events.EventType.CLICK,
+        this.onDeleteClick, false, this);
+
     goog.dom.appendChild(row, thumb);
     goog.dom.appendChild(row, caption);
+    goog.dom.appendChild(row, deleteButton);
     goog.dom.appendChild(row, hiddenField);
 
     // Add to the container
@@ -39,7 +49,19 @@ sean.test.ThumbViewer.prototype.createRow = function(e) {
     this.rowCount_ += 1;
 };
 
-sean.test.ThumbViewer.prototype.deleteRow = function(id) {
-    rows = this.getElement().getElementsByTagName('div');
-    goog.dom.removeNode(rows[id]);
+sean.test.ThumbViewer.prototype.onDeleteClick = function(e) {
+    var row = e.target.parentNode;
+    goog.dom.removeNode(row);
+};
+
+sean.test.ThumbViewer.prototype.exitDocument_ = function() {
+    goog.ui.ThumbViewer.superClass_.exitDocument.call(this);
+
+    // Remove the delete listeners
+    var rows = this.getElement().getElementsByTagName('div');
+    for (i = 0; i < rows.length; i++) {
+        var btn = rows[i].getElementById('thumb_viewer_row_delete_' + i);
+        goog.events.unlisten(btn, goog.events.EventType.CLICK,
+            this.onDeleteClick, false, this);
+    }
 };
